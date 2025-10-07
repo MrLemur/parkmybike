e48a188dcb031a21a853442be954f1f620e162e2
Kassim <17055328+MrLemur@users.noreply.github.com>
2025-10-07 18:13:35 +0100

    fix: remove proxy option

--- react/vite.config.js
+++ react/vite.config.js
@@ -6,26 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true,
-    proxy: {
-        '/api': {
-          target: 'http://localhost:5001',
-          changeOrigin: false,
-          secure: false,      
-          ws: true,
-          configure: (proxy, _options) => {
-            proxy.on('error', (err, _req, _res) => {
-              console.log('proxy error', err);
-            });
-            proxy.on('proxyReq', (proxyReq, req, _res) => {
-              console.log('Sending Request to the Target:', req.method, req.url);
-            });
-            proxy.on('proxyRes', (proxyRes, req, _res) => {
-              console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-            });
-          },
-        }
-      }
+    open: true
   },
   build: {
     outDir: 'build'
